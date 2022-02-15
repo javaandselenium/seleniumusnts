e230fd29da3a802b04950e5e01ae5927016341f4
QSP <QSP@QSP>
2022-02-15 07:54:44 +0530

    Locators

---
+++ src/locators/Test1.java
@@ -0,0 +1,14 @@
+package locators;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	
+
+	}
+
+}
