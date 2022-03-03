864dd5f4995e8b277e3e6a0b039b7bbb5a1241c6
QSP <QSP@QSP>
2022-03-03 07:57:56 +0530

    Popup-2

---
+++ src/popups/Test5.java
@@ -0,0 +1,28 @@
+package popups;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://skillrary.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	
+		//driver.switchTo().newWindow(WindowType.TAB);
+		driver.switchTo().newWindow(WindowType.WINDOW);
+		
+		driver.get("https://www.amazon.com/");
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("phone");
+	
+		driver.quit();
+	}
+
+}
